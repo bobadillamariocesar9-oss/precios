e3c6af224ae6de9e443ffafad9ea864e7eff887e
bobadillamariocesar9-oss <bobadillamariocesar9@gmail.com>
2026-09-23 18:27:50 -0300

    test: align Store test accessors with baseUrl property

--- src/test/java/com/apiprecios/repository/FavoriteRepositoryTest.java
+++ src/test/java/com/apiprecios/repository/FavoriteRepositoryTest.java
@@ -50,7 +50,7 @@ class FavoriteRepositoryTest extends AbstractRepositoryTest {
                 .username("ana").email("ana@test.com").password("pass").build());
 
         Store store = storeRepository.save(Store.builder()
-                .name("MercadoLibre").url("https://ml.com").build());
+                .name("MercadoLibre").baseUrl("https://ml.com").build());
 
         notebook = productRepository.save(Product.builder()
                 .name("Notebook").store(store).build());
